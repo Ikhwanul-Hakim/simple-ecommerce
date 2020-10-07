91d1d74da2960fa7b9447b0d7679a8c3ef042a42
Ikhwanul-Hakim <ikh.hakim@gmail.com>
2020-10-07 11:14:05 +0700

    setup POSTMAN

--- controller/category.js
+++ controller/category.js
@@ -1,19 +1,137 @@
+const { request } = require('express')
+const responseBuilder = require('../helper/response')
+
 module.exports.getAll = (req, res) => {
-    res.send('not implemented : get all category')
+    // const response = responseBuilder('ERROR', 'not implemented : get all category',)
+    const sql = 'SELECT * FROM `category` WHERE deleted_at IS NULL'
+
+    req.db.query(sql, (err, result) => {
+        if (err){
+            res.send(
+                responseBuilder(0, err.message)
+            )
+            console.log(err.message);
+        } else {
+            console.log('RESULT : ', result);
+            res.send(
+                responseBuilder(1, null, result)
+            )
+        }
+    })
 }
 
 module.exports.getProduct = (req, res) => {
-    res.send('not implemented : get product by category')
+    const sql = 'SELECT * FROM `category` WHERE id = ? AND deleted_at IS NULL'
+
+    req.db.query(sql, req.params.id, (err, result) => {
+        if (err){
+            res.send(
+                responseBuilder(0, err.message)
+            )
+            console.log(err.message);
+        } else {
+            const sql2 = 'SELECT * FROM `product` WHERE id_category = ? AND deleted_at IS NULL'
+            req.db.query(sql2, req.params.id, (err2, result2) => {
+                if (err2){
+                    res.send(
+                        responseBuilder(0, err2.message) 
+                    )
+                    console.log(err2.message);
+                    } else {                                
+            console.log('RESULT : ', result2);
+            res.send(
+                responseBuilder(1, null, {...result[0], products: result2})
+            )
+        }
+    })
+    }
+})
 }
 
 module.exports.insert = (req, res) => {
-    res.send('not implemented : insert category')
+    if (req.body.name){
+        const sql = 'INSERT INTO  `category` (`name`) VALUES (?)'        
+    req.db.query(sql, req.body.name, (err, result) => {
+        if (err){
+            res.send(
+                responseBuilder(0, err.message)
+            )
+            console.log(err.message);
+        } else {
+            console.log('RESULT : ', result);
+            const sql2 = 'SELECT * FROM `category` WHERE `id` = ?'
+            req.db.query(sql2, result.insertId, (err2, result2) => {
+                if (err2){
+                    res.send(
+                        responseBuilder(0, err2.message)
+                    )
+                    console.log(err2.message);
+                } else {
+                    res.send(
+                        responseBuilder(1, null, result2[0])
+                    )
+                }
+            })
+        }
+    })
+    } else {
+        res.send(
+            responseBuilder(0, 'property name is required')
+        )
+
+    }
 }
 
-module.exports.update = (req, res) => {
-    res.send('not implemented : update category')
+module.exports.update = (req, res) => {    
+    const sql = 'UPDATE `category` SET `name` = ?, updated_at = NOW() WHERE id = ?'    
+    req.db.query(sql, [req.body.name, req.params.id], (err, result) => {
+        if (err){
+            res.send(
+                responseBuilder(0, err.message)
+            )
+            console.log(err.message);
+        } else {
+            console.log('RESULT : ', result);
+            const sql2 = 'SELECT * FROM `category` WHERE `id` = ?'
+            req.db.query(sql2, req.params.id, (err2, result2) => {
+                if (err2){
+                    res.send(
+                        responseBuilder(0, err2.message)
+                    )
+                    console.log(err2.message);
+                } else {
+                    res.send(
+                        responseBuilder(1, null, result2[0])
+                    )
+                }
+            })
+        }
+    })
 }
 
 module.exports.delete = (req, res) => {
-    res.send('not implemented : delete category')
+    const sql = 'UPDATE `category` SET updated_at = NOW(), deleted_at = NOW() WHERE id = ?'    
+    req.db.query(sql, req.params.id, (err, result) => {
+        if (err){
+            res.send(
+                responseBuilder(0, err.message)
+            )
+            console.log(err.message);
+        } else {
+            console.log('RESULT : ', result);
+            const sql2 = 'SELECT * FROM `category` WHERE `id` = ?'
+            req.db.query(sql2, req.params.id, (err2, result2) => {
+                if (err2){
+                    res.send(
+                        responseBuilder(0, err2.message)
+                    )
+                    console.log(err2.message);
+                } else {
+                    res.send(
+                        responseBuilder(1, null, result2[0])
+                    )
+                }
+            })
+        }
+    })
 }
